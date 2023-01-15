c619bd3cf25a553b2f3d659c96f20d1168054fe7
smark-ark <arkhangreat2@gmail.com>
2023-01-15 18:46:42 +0530

    installed apps

--- portfolio/person/models.py
+++ portfolio/person/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=300)
+    role = models.CharField(max_length=100)
+    tagline = models.CharField(max_length=100)
+    about = models.TextField()
+    description = models.TextField()
+    display_image = models.CharField(max_length=300)
--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "person",
+    "events",
+    "experience",
+    "projects",
 ]
 
 MIDDLEWARE = [
